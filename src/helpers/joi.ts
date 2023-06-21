02935ec031402a7c04440e5c84983e7a8dd2684b
CREDO23 <thierrybakera12@gmail.com>
2023-06-21 14:01:38 +0200

    feat: add the authRouter (register and login)

---
+++ src/helpers/joi.ts
@@ -0,0 +1,27 @@
+import * as joi from 'joi';
+
+export class JOIUserValidation {
+    static register = joi.object({
+        username: joi.string().required(),
+        email: joi.string().required().email(),
+        password: joi.string().required(),
+        phoneNumber: joi.string(),
+    });
+
+    static login = joi.object({
+        username: joi.string().required(),
+        password: joi.string().required(),
+    });
+
+    static update = joi.object({
+        email: joi.string().email(),
+        password: joi.string(),
+        phoneNumber: joi.string(),
+        firstname: joi.string(),
+        lastname: joi.string(),
+        country: joi.string(),
+        city: joi.string(),
+        adress_line1: joi.string(),
+        adress_line2: joi.string(),
+    });
+}
